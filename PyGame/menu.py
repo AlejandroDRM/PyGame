83a4082c3c77316cda6410f8ba849923c895e382
AlejandroDRM <alejandro.rivero88@gmail.com>
2016-04-19 13:51:38 -0300

    Menu Start Quit

--- PyGame/menu.py
+++ PyGame/menu.py
@@ -25,14 +25,25 @@ class GameMenu():
     def __init__(self, screen, items, bg_color=(0,0,0), font=None, font_size=30,
                     font_color=(255, 255, 255)):
         self.screen = screen
+        self.scr_width = self.screen.get_rect().width
+        self.scr_height = self.screen.get_rect().height
         self.bg_color = bg_color
         self.clock = pygame.time.Clock()
         self.font = pygame.font.SysFont(font, font_size)
         self.font_color = font_color
         self.items = []
-        for item in items:
+        for index, item in enumerate(items):
             label = self.font.render(item, 1, font_color)
-            self.items.append(label)
+ 
+            width = label.get_rect().width
+            height = label.get_rect().height
+ 
+            posx = (self.scr_width / 2) - (width / 2)
+            # t_h: total height of text block
+            t_h = len(items) * height
+            posy = (self.scr_height / 2) - (t_h / 2) + (index * height)
+ 
+            self.items.append([item, label, (width, height), (posx, posy)])
  
     def run(self):
         mainloop = True
@@ -47,8 +58,8 @@ class GameMenu():
  
             # Redraw the background
             self.screen.fill(self.bg_color)
-            for label in self.items:
-                self.screen.blit(label, (100, 100))
+            for name, label, (width, height), (posx, posy) in self.items:
+                self.screen.blit(label, (posx, posy))
             pygame.display.flip()
             
 # ------------------------------
